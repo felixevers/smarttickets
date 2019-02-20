82283649399355dcc9c47419b6cbb7ee503bab9d
USE-TO <use-to@outlook.de>
2019-02-20 21:44:15 +0100

    fixed angular routing

--- app/resources/render.py
+++ app/resources/render.py
@@ -8,9 +8,12 @@ def register_render(app: Flask):
     def render_files(path):
         return send_from_directory(config["FRONTEND"], path)
 
-    @app.route('/f/', defaults={'path': ''})
-    @app.route('/f/<path:path>')
-    def render_index(path):
+    @app.route('/f/')
+    def render_index():
+        return send_from_directory(config["FRONTEND"], 'index.html')
+        
+    @app.errorhandler(404)
+    def render_all_index(e):
         return send_from_directory(config["FRONTEND"], 'index.html')
 
     @app.route('/')
